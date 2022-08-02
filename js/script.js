06be3eaa9bf50a0f77c5239a962d42ecc4dd6243
Kim <tjdgns6120@gmail.com>
2022-08-02 18:16:57 +0900

    0802 - 헤드 4단계 반응형 진행 필요

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
+/* header */
 var h_no;
 var h_h
 $(".gnb_title li").hover(function(){
     h_no = $(this).index();
     $("#detail .wrap > div").eq(h_no).addClass("on").siblings("div").removeClass("on")
     h_h = $("#detail .wrap > div").eq(h_no).outerHeight(true);
-    console.log(h_h);
     $("#detail").stop().animate({"height":h_h},500);
     $(".modal").addClass("on");
 
-},function(){
+    $(this).addClass("on").children(":after");
+    $(this).siblings("li").removeClass("on");
+})
+$(".gnb").mouseleave(function(){
     $("#detail .wrap > div").siblings("div").removeClass("on")
-    $("#detail").stop().animate({"height":0},500);
+    $("#detail").stop().css({"height":0});
     $(".modal").removeClass("on");
+    $(".gnb_title > li").removeClass("on");
+})
+/* 반복문 이미지 교체 */
+var p_no = $(".part a:last-child").index();
+var i;
+for(i = 0; i <= p_no; i++){
+$(".part .category a").eq(i).children().css({"background-image":"url(images/h"+(i+1)+".png)"});
+};
+$(".part .category a").hover(function(){
+    bp = $(this).index() + 1;
+    $(this).children().css({"background-image":"url(images/hb"+bp+".png)"});
+},
+function(){
+    $(this).children().css({"background-image":"url(images/h"+bp+".png)"});
+})
+
+$(".head_icon button").click(function(){
+    $(this).toggleClass("on");
+})
+$("header .fnb .side p").click(function(){
+    $(this).toggleClass("on");
 })
